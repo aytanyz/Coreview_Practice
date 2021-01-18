d97a8f9cf346176740785c41fe4ccbfc8292159f
aytanyz <ayteny_z@mail.ru>
2021-01-18 22:28:49 +0100

    updates

---
+++ ACM/src/ACM.BL/Customer.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+        }
+
+        public int CustomerId{ get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName{ get; set; }
+        public static int InstanceCount { get; set; }
+        public string FullName 
+        {
+            get
+            {
+                string fullName = LastName;
+                if(!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if(!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        } 
+        private string _lastName;
+        public string LastName
+        {
+            set 
+            {
+                LastName = value;
+            }
+            get
+            {
+                return _lastName;
+            }
+        }
+        
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName))
+                isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress))
+                isValid = false;
+            return isValid;
+        }
+    }
+}
---
+++ ACM/src/ACM.BL/OrderItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (Quantity <=0)
+                isValid = false;
+            if (ProductId <= 0)
+                isValid = false;
+            if (PurchasePrice == null)
+                isValid = false;
+            return isValid;
+        }
+
+        public OrderItem Retreive(int orderId)
+        {
+            return new OrderItem();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+
+}
